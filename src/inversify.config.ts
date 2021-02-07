8501f09dbb22b7fef13d8311ae9070e47c57a34a
lewjc <lewisjcummins@hotmail.co.uk>
2021-02-07 15:35:35 +0000

    Add settings inversify registrations

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -4,6 +4,7 @@ import { SERVICE_TYPES } from "./service/inversify.types";
 import { DB_TYPES } from "./db/inversify.types";
 import { MESSAGE_TYPES } from "./messages/inversify.types";
 import { SOCKET_TYPES } from "./socket/inversify.types";
+import { SETTINGS_TYPES } from "./settings/inversify.types";
 
 import ServiceManagement from "./service/interfaces/ServiceManagement";
 import AggregatorDatabase from "./db/AggregatorDatabase";
@@ -17,8 +18,16 @@ import MessageManagement from "./messages/interfaces/MessageManagement";
 import SocketHub from "./socket/interfaces/SocketHub";
 import SocketManager from "./socket/SocketManager";
 import ServiceManager from "./service/ServiceManager";
+import SettingsDataHandler from "./db/interfaces/SettingsDataHandler";
+import SettingsBridge from "./db/SettingsBridge";
+import SettingsManagement from "./settings/interfaces/SettingsManagement";
+import SettingsManager from "./settings/SettingsManager";
 
 const Resolver = new Container();
+
+/** SCOPED */
+
+// Management Layer
 
 Resolver.bind<ServiceManagement>(SERVICE_TYPES.ServiceManager).to(
   ServiceManager
@@ -26,11 +35,21 @@ Resolver.bind<ServiceManagement>(SERVICE_TYPES.ServiceManager).to(
 Resolver.bind<MessageManagement>(MESSAGE_TYPES.MessageManager).to(
   MessageManager
 );
+Resolver.bind<SettingsManagement>(SETTINGS_TYPES.SettingsManager).to(
+  SettingsManager
+);
+
+// Bridge Layer
+
 Resolver.bind<MessageDataHandler>(DB_TYPES.MessageBridge).to(MessageBridge);
 Resolver.bind<ServiceDataHandler>(DB_TYPES.ServiceBridge).to(ServiceBridge);
+Resolver.bind<SettingsDataHandler>(DB_TYPES.SettingsBridge).to(SettingsBridge);
+
 Resolver.bind<DatabaseContext>(DB_TYPES.AggreagtorDatabase).to(
   AggregatorDatabase
 );
+
+/** SINGLETON */
 
 Resolver.bind<SocketHub>(SOCKET_TYPES.SocketManager)
   .to(SocketManager)
